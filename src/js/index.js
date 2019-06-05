20b48559b531d55d68048346618d632209eed408
evgenyzhernoklev <oldschool127@mail.ru>
2019-06-05 19:26:44 +0300

    Add Gulp as task manager

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+import XLSX   from "xlsx";
+import Parser from "./parser.js";
+
+let form    = document.body.querySelector('.form'),
+    field   = form.querySelector('.form__field');
+
+function to_json(workbook) {
+  var result = {};
+
+  workbook.SheetNames.forEach(function(sheetName) {
+    var roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {header:1});
+    if(roa.length) result[sheetName] = roa;
+  });
+  return JSON.stringify(result, 2, 2);
+};
+
+function handleFile(e) {
+  var files = e.target.files, f = files[0],
+      reader = new FileReader();
+
+  reader.onload = function(e) {
+    var data = new Uint8Array(e.target.result),
+        workbook = XLSX.read(data, {type: 'array'});
+
+    /* DO SOMETHING WITH workbook HERE */
+    let json = to_json(workbook),
+        obj_list = JSON.parse(json);
+
+    for (let key in obj_list) {
+      new Parser(obj_list[key]);
+    }
+  };
+  reader.readAsArrayBuffer(f);
+}
+
+field.addEventListener('change', handleFile, false);
---
+++ src/js/parser.js
@@ -0,0 +1,297 @@
+"use strict";
+
+class Parser {
+  constructor(array) {
+    this.array = array;
+    this.bountyKillersData = {
+      "header": {
+        "title": "",
+        "subtitle": ""
+      },
+      "nav": [
+
+      ],
+      "killers": [
+
+      ]
+    };
+    this.current_tab = -1;
+    this.group = [];
+    this.hashes = [];
+    this.init();
+  }
+
+  init() {
+    this.parseArray(this.array);
+  }
+
+  parseArray(array) {
+    let self = this,
+        checkRender = function() {
+          if (self.group.length > 1) {
+            self.renderAsGroup(self.group);
+          } else {
+            if (self.group.length == 1) {
+              self.renderAsSingle(self.group);
+            }
+          }
+        };
+
+    array.forEach(function(item, index, array) {
+      let unused_strings = /Первая строка|Вторая строка|Код для заказа/i;
+
+      if ( unused_strings.test(item[0]) ) { return false; }
+
+      if (item.length == 0) {
+        checkRender();
+        self.group = [];
+      } else if (item.length == 1) {
+        checkRender();
+
+        if (array[index - 1] && array[index - 1].length == 1) {
+          self.parseAsCondition(item);
+        } else {
+          self.parseAsTitle(item);
+        }
+
+        self.group = [];
+      } else if (array[index - 1]) {
+        if (self.is_oneGroup(array[index - 1], array[index])) {
+          self.group.push(item);
+
+          if (index == (array.length - 1)) {
+            self.renderAsGroup(self.group);
+          }
+        } else {
+          checkRender();
+          self.group = [];
+          self.group.push(item);
+
+          if (index == (array.length - 1)) {
+            self.renderAsSingle(self.group);
+          }
+        }
+      }
+    });
+
+    if (this.bountyKillersData["nav"].length < 2) {
+      this.bountyKillersData["nav"] = [];
+    }
+
+    this.prepareFile();
+    console.log(this.bountyKillersData);
+  }
+
+  parseAsCondition(item) {
+    const SPACE = 2; // 2 = symbol + space
+    let title = item[0],
+        condition = "";
+
+    condition = title.search( /:|!/i );
+    condition = title.slice(condition + SPACE);
+    this.bountyKillersData["killers"][this.current_tab]["condition"] = condition;
+  }
+
+  parseAsTitle(item) {
+    const SPACE = 2; // symbol + space
+    let text = item[0],
+        hash = "",
+        tab_name_end_position = text.search( /\.|:/i ),
+        tab_name = text.slice(0, tab_name_end_position),
+        title = "",
+        condition = "";
+
+    if (tab_name_end_position > 0) {
+      text = text.slice(tab_name_end_position + SPACE);
+    }
+
+    let title_end_position = text.search( /\./i );
+
+    if (title_end_position > 0) {
+      title = text.slice(0, title_end_position + SPACE/2);
+      condition = text.slice(title_end_position + SPACE);
+    } else {
+      title = text;
+    }
+
+    switch(tab_name) {
+      case "Макияж":
+      case "Все для макияжа":
+        hash = "makeup";
+        break;
+      case "Ароматы":
+        hash = "fragrance";
+        break;
+      case "Уход":
+        hash = "care";
+        break;
+      case "Уход за лицом":
+      case "Средства по уходу за лицом":
+        hash = "face";
+        break;
+      case "Мода и стиль":
+        hash = "style";
+        break;
+    }
+
+    if ( ~this.hashes.indexOf(hash) ) {
+      this.addSubsection(title, condition);
+      return false;
+    }
+
+    this.hashes.push(hash);
+
+    let new_section = {
+      "lines": [
+        {
+          "title": title,
+          "condition": condition,
+          "rows": 3,
+          "offers": [
+
+          ]
+        }
+      ],
+      "condition": ""
+    };
+    let new_tab = {
+      "navText": tab_name,
+      "navHash": hash
+    };
+
+    this.bountyKillersData["nav"].push(new_tab);
+    this.bountyKillersData["killers"].push(new_section);
+    this.current_tab += 1;
+  }
+
+  addSubsection(title, condition) {
+    let new_subsection = {
+      "title": title,
+      "condition": condition,
+      "rows": 3,
+      "offers": [
+
+      ]
+    };
+
+    this.bountyKillersData["killers"][this.current_tab]["lines"].push(new_subsection);
+  }
+
+  is_oneGroup(item_prev, item) {
+    let is_oneGroup = true;
+
+    if (item_prev[1] != item[1] || item_prev[3] != item[3] || item_prev[10] != item[10]) { // profile code, name, actual cost
+      is_oneGroup = false;
+    }
+
+    return is_oneGroup;
+  }
+
+  renderAsGroup(group) {
+    let item_rendering = group[0],
+        item_type = item_rendering[6],
+        title = "",
+        chosenTitle = "";
+
+    if ( item_type.search(/\d/) != -1 ) {
+      item_type = "sizes";
+    } else {
+      item_type = "colors";
+    }
+
+    switch(item_type) {
+      case "sizes":
+        title = "Размеры";
+        chosenTitle = "Выбранный размер";
+        break;
+      case "colors":
+        title = "Оттенки";
+        chosenTitle = "Выбранный оттенок";
+        break;
+    }
+
+    let item_list = {
+      "ln": [
+      ],
+      "price": {
+          "actualCostRub": item_rendering[10],
+          "oldCostRub": item_rendering[8]
+      },
+      "variantsType": item_type,
+      "variants": {
+          "title": title,
+          "chosenTitle": chosenTitle,
+          "variantsText": [
+          ]
+      },
+      "description": item_rendering[4] || "",
+      "label": item_rendering[12],
+      "boldTitle": item_rendering[3] || ""
+    };
+
+    group.forEach(function(item, index, group) {
+      item_list["ln"].push(item[0]);
+      item_list["variants"]["variantsText"].push(item[6]);
+    });
+
+    let lines_array = this.bountyKillersData["killers"][this.current_tab]["lines"];
+    lines_array[lines_array.length - 1]["offers"].push(item_list);
+  }
+
+  renderAsSingle(group) {
+    let item_rendering = group[0];
+
+    if (item_rendering.length < 1) { return false; }
+
+    let item_type = item_rendering[6];
+
+    if (item_type) {
+      this.renderAsGroup(group);
+      return false;
+    }
+
+    let item_single = {
+      "price": {
+        "actualCostRub": item_rendering[10],
+        "oldCostRub": item_rendering[8]
+      },
+      "description": item_rendering[4] || "",
+      "ln": item_rendering[0],
+      "label": item_rendering[12],
+      "boldTitle": item_rendering[3] || ""
+    };
+    let lines_array = this.bountyKillersData["killers"][this.current_tab]["lines"];
+    lines_array[lines_array.length - 1]["offers"].push(item_single);
+  }
+
+  prepareFile() {
+    let file_data = "";
+
+    file_data += "\"use strict\";\n\n";
+    file_data += "var bountyKillersData = ";
+    file_data += JSON.stringify(this.bountyKillersData, null, '\t');
+    this.download(file_data, 'products.js', 'text/plain');
+  }
+
+  download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+
+    if (window.navigator.msSaveOrOpenBlob) { // IE10+
+      window.navigator.msSaveOrOpenBlob(file, filename);
+    } else { // Others
+      var a = document.createElement("a"),
+          url = URL.createObjectURL(file);
+
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function() {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+      }, 0);
+    }
+  }
+}
+
+export default Parser;
